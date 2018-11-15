f465e2a6bd5a036cb2cc7d6547c37323b9cfd9c5
Luca <lcpp1994@gmail.com>
2018-11-15 22:37:25 +0100

    minor chart refactoring

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,9 +1,9 @@
+import { ExamResult } from './../../interfaces';
 import { chartConfig } from './chart.config';
-import { Component, AfterViewInit, Input, OnInit, OnDestroy } from '@angular/core';
+import { Component, AfterViewInit, Input, OnDestroy } from '@angular/core';
 import * as c3 from 'c3';
 import { DataService } from '../../services/data/data.service';
 import { Observable, Subscription } from 'rxjs';
-import { ExamResult } from '../../interfaces';
 import { format } from 'date-fns';
 
 
@@ -12,47 +12,48 @@ import { format } from 'date-fns';
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent implements AfterViewInit, OnInit, OnDestroy {
+export class ChartComponent implements AfterViewInit, OnDestroy {
 
   @Input() exam: string;
 
   chart: c3.ChartAPI;
-  data: Observable<ExamResult[]>;
   subscriptions: Subscription[];
 
   constructor(public dataService: DataService) {
     this.subscriptions = [];
   }
 
-  ngOnInit() {
-    this.data = this.dataService.getExamResults(this.exam)
-  }
-
   ngAfterViewInit() {
     this.chart = c3.generate(chartConfig());
     this.subscriptions.push(
-      this.data.subscribe(data => {
-        this.chart.load({
-          columns: [
-            ['x', ...data.map(x => format(new Date(x.date.seconds * 1000), 'YYYY-MM-DD'))],
-            ['results', ...data.map(x => x.result)]
-          ],
-          unload: ['x', 'results']
-        })
-      }),
+      this.dataService.getExamResults(this.exam)
+        .subscribe(data => this.loadChart(data)),
       this.dataService.getExamDetails(this.exam)
-        .subscribe(data => {
-          this.chart.regions([{
-            axis: 'y',
-            start: data.min,
-            end: data.max,
-          }])
-        })
+        .subscribe(data => this.loadChartRegions(data))
     )
   }
 
   ngOnDestroy() {
     this.subscriptions.forEach(x => x.unsubscribe());
+    this.chart.destroy();
+  }
+
+  loadChart(data: ExamResult[]) {
+    this.chart.load({
+      columns: [
+        ['x', ...data.map(x => format(new Date(x.date.seconds * 1000), 'YYYY-MM-DD'))],
+        ['results', ...data.map(x => x.result)]
+      ],
+      unload: ['x', 'results']
+    })
+  }
+
+  loadChartRegions(data) {
+    this.chart.regions([{
+      axis: 'y',
+      start: data.min,
+      end: data.max,
+    }])
   }
 
 }
