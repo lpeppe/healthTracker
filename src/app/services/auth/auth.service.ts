34d70db387ef175f265d3cf206ef5ce76ab7f0ee
Luca <lcpp1994@gmail.com>
2018-11-15 22:38:05 +0100

    improved home style

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -16,15 +16,11 @@ export class AuthService implements OnDestroy {
     this.subscriptions = [];
     this.subscriptions.push(
       this.afAuth.authState
-        .subscribe(data => {
-          this.user = data
-          console.log(this.user)
-        })
+        .subscribe(data => this.user = data)
     );
   }
 
   login(): Promise<any> {
-    console.log(this.platform.platforms())
     if (this.platform.is("android") || this.platform.is("ios"))
       return this.afAuth.auth.signInWithRedirect(new auth.GoogleAuthProvider());
     return this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
